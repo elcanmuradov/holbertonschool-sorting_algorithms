563726a2505ad145960f0b10324604d61d3f122f
elcanmuradov <elcanmuradov25@gmail.com>
2025-08-05 14:17:13 +0000

    3

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,25 +1,47 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - funct
- * @list: arg1
-*/
+ * swap_nodes - Swaps two adjacent nodes in a doubly linked list
+ * @list: Double pointer to the head of the list
+ * @node1: First node to swap
+ * @node2: Second node to swap
+ */
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+{
+iif (!node1 || !node2)
+return;
+if (node1->prev)
+node1->prev->next = node2;
+else
+*list = node2;
+if (node2->next)
+node2->next->prev = node1;
+node2->prev = node1->prev;
+node1->next = node2->next;
+node1->prev = node2;
+node2->next = node1;
+}
 
+/**
+ * insertion_sort_list - Sorts a doubly linked list using insertion sort
+ * @list: Double pointer to the head of the list
+ */
 void insertion_sort_list(listint_t **list)
 {
-int i, temp; 
-listint_t *complist;
-i = 0;
-while (*list)
+listint_t *current, *temp;
+
+if (!list || !*list || !(*list)->next)
+return;
+current = (*list)->next;
+while (current)
 {
-if (*list->n > *list->next->n)
+temp = current->next;
+while (current->prev && current->n < current->prev->n)
 {
-temp = *list->n;
-complist->n = *list->next->n;
-complist->next->n = *temp;
+swap_nodes(list, current->prev, current);
+print_list(*list);
 }
-*list = *list->next;
-i++;
-print_list(complist);
+
+current = temp;
 }
 }
