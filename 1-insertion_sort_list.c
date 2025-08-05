710c9e909dc0c995c7788ac83b98a8fcdd042e68
elcanmuradov <elcanmuradov25@gmail.com>
2025-08-05 14:09:11 +0000

    3

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,17 +8,17 @@
 void insertion_sort_list(listint_t **list)
 {
 int i, temp; 
-listint_t complist;
+listint_t *complist;
 i = 0;
 while (*list)
 {
-if (list->n > list->next->n)
+if (*list->n > *list->next->n)
 {
-temp = list->n;
-complist->n = list->next->n;
-complist->next->n = temp;
+temp = *list->n;
+complist->n = *list->next->n;
+complist->next->n = *temp;
 }
-list = list->next;
+*list = *list->next;
 i++;
 print_list(complist);
 }
