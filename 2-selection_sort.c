1a16de7584afc8620da2ecfddc6266589eb402f2
elcanmuradov <elcanmuradov25@gmail.com>
2025-08-06 13:39:09 +0000

    3

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorting algorithm function
+ * array: arg1
+ * size: size of array
+*/
+
+void selection_sort(int *array, size_t size)
+{
+int j, i = 0;
+int k, temp2;
+int temp = array[0];
+while (j < size)
+{
+while (i < size)
+{
+if (temp > array[i])
+{
+temp = array[i];
+k = i;
+}
+i++;
+}
+
+temp2 = array[j];
+array[j] = temp;
+array[k] = temp2;
+
+print_array(array, size_;
+i = j++;
+j++;
+}
